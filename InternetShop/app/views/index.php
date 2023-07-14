e43d86c9f40670958d006fca3c7d92bd3258c21f
Nazar <90456305+nazarskrinskyi@users.noreply.github.com>
2023-07-14 10:58:17 +0300

    Delete InternetShop directory

--- InternetShop/app/views/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title = "Home Page";
-ob_start(); ?>
-
-    <h1>Home</h1>
-    <script type="text/javascript" src="js/sidebarCollapse.js"></script>
-<?php $content = ob_get_clean();
-include_once "app/views/layout.php";
